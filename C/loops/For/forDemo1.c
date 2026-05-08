f866cf8e141218a2d4aae058e7b4bbb737433b0d
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-08 13:35:15 +0530

    for

---
+++ C/loops/For/forDemo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a;
+
+    // clrscr();
+
+    for(a = 0; a <= 5; a++){
+
+        printf("hello world\n");
+    }
+
+    // getch();
+}
